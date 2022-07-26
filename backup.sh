519276e0899159e8ec5803aeb0b9e8eb1fe49c46
hp27596 <hp27596@gmail.com>
2022-07-26 12:01:27 +0700

    Initial Commit

---
+++ backup.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# check to see is git command line installed in this machine
+IS_GIT_AVAILABLE="$(git --version)"
+if [[ $IS_GIT_AVAILABLE == *"version"* ]]; then
+  echo "Git is Available"
+else
+  echo "Git is not installed"
+  exit 1
+fi
+
+
+## copy .config files
+cp --parents -r /etc/X11/xorg.conf.d/ $HOME/awesomebackup
+
+#cp -p -r ~/config-alacritty $HOME/dotfiles
+cp -p -r ~/.config/{misc,alacritty,bottom,btop,cmus,dunst,fcitx5,feh,'gtk-2.0','gtk-3.0',mpv,nvim,picom,qtile,ranger,dmenu-frecency,zathura} $HOME/awesomebackup/.config
+
+cp -r ~/.doom.d/ $HOME/awesomebackup/
+# cp -r ~/.fonts/ $HOME/awesomebackup/
+
+cp /etc/{environment,pacman.conf,vconsole.conf} $HOME/awesomebackup/etc/
+
+# copy other dot files
+cp $HOME/{'.gtkrc-2.0',.vimrc,.zshrc,.xprofile,.Xresources,.tmux.conf,.ticker.yaml} $HOME/awesomebackup
+
+# Check git status
+gs="$(git status | grep -i "modified")"
+# echo "${gs}"
+
+# If there is a new change
+if [[ $gs == *"modified"* ]]; then
+  echo "push"
+fi
+
+
+# push to Github
+git add -A;
+git commit -m "New backup `date +'%Y-%m-%d %H:%M:%S'`";
+git push origin main
